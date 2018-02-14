52334c13373ab839928fdfd5bf6d6eaa75d07465
Oscar Falkman <ofalkman@gmail.com>
2018-02-14 09:53:10 +0100

    Resting ready for testing

--- src/main/java/view/entrypoints/Courses.java
+++ src/main/java/view/entrypoints/Courses.java
@@ -1,13 +1,11 @@
 package view.entrypoints;
 
 import common.CourseDTO;
+import common.ReviewDTO;
 import controller.Controller;
-import view.rest.CourseRest;
-import view.rest.ReviewRest;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
 import java.util.List;
 
 @Path("/courses")
@@ -18,43 +16,28 @@ public class Courses {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public void createCourse(CourseRest courseRequest) {
-        CourseDTO courseDTO = new CourseDTO(courseRequest.getName(), courseRequest.getSchool());
-        controller.createCourse(courseDTO);
+    public void createCourse(CourseDTO courseRequest) {
+        controller.createCourse(courseRequest);
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<CourseRest> getCourses() {
-        List<CourseDTO> dtoList = controller.getCourses();
-        List<CourseRest> courseRests = new ArrayList<CourseRest>();
-        for(CourseDTO dto : dtoList)
-            courseRests.add(new CourseRest(dto.getName(), dto.getSchool()));
-        return courseRests;
+    public List<CourseDTO> getCourses() {
+        return controller.getCourses();
     }
 
     @GET
     @Path("/{name}")
     @Produces(MediaType.APPLICATION_JSON)
-    public CourseRest getCourse(@PathParam("name") String name) {
-        CourseDTO courseDTO = controller.getCourse(name);
-        return new CourseRest(courseDTO.getName(), courseDTO.getSchool());
+    public CourseDTO getCourse(@PathParam("name") String name) {
+        return controller.getCourse(name);
     }
 
     @GET
     @Path("/reviews")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<ReviewRest> getReviews() {
-        return controller.getReviews;
-    }
-
-    @PUT
-    @Path("/{name}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public void updateCourse(@PathParam("name") String name, CourseRest course) {
-        CourseDTO courseDTO = new CourseDTO(course.getName(), course.getSchool());
-        controller.updateCourse(name, courseDTO);
+    public List<ReviewDTO> getReviews() {
+        return controller.getReviews();
     }
 
 }
